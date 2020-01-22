386d68c154be9e4a339b7dfa9502ac932d45122f
well-balanced <softkey95@gmail.com>
2020-01-23 00:16:35 +0900

    Create Server

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
