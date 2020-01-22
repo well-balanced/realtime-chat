3470786116f446f142c8c871d49cb9295e8d9bf7
well-balanced <softkey95@gmail.com>
2020-01-23 01:09:37 +0900

    Create communicate function that connect between a client and a server

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+io.emit('some event', { 
+    someProperty: 'some value', 
+    otherProperty: 'other value'
+})
+
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    })
 });
 
 http.listen(3000, () => {
